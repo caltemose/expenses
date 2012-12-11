c75f9317ffe9a0495b006e1f97c5b94f65c97067
Chad Altemose <chad@chadzilla.com>
2012-12-11 09:49:11 -0500

    building js modules

--- production/app/scripts/main.js
+++ production/app/scripts/main.js
@@ -33,26 +33,47 @@ TRANSACTIONS.ui handles the ui display
 TRANSACTIONS.ui = (function() {
 	'use strict';
 
-	//private vars & methods
-	var $container;
+    // [ private properties ]
+	var containerId, $container;
 
+    // [ private methods ]
 	function draw() {
-		$container.html("TRANSACTIONS.display");
-	}
-
-	//public interface
-	return {
-		init: function(element){
-			$container = element;
-			draw();
+		var records = TRANSACTIONS.model.getRecords(),
+			record,
+			i;
+		//@TODO remove existing ul.records
+		$container.html('<ul class="records"></ul>');
+		for(i=0;i<records.length;i++) {
+			$('#' + containerId + ' ul.records').append( getRecordHtml(records[i]) );
 		}
 	}
+
+	function getRecordHtml(record) {
+		//date,amount,payee,category
+		var html = '<li>';
+		html += '<a class="save-button" href="#">[save]</a>';
+		html += '<input type="text" size="11" maxlength="10" name="date" value="' + record.date + '" />';
+		html += '<input type="text" size="50" maxlength="255" name="payee" value="' + record.payee + '" />';
+		html += '<input type="number" size="11" maxlength="12" name="amount" value="' + record.amount + '" />';
+		html += '<input type="text" size="30" maxlength="64" name="category" data-id="category id" value="' + record.category + '" />';
+		html += '</li>';
+		return html;
+	}
+
+    // [ public methods ]
+	return {
+		init: function(elementId){
+			containerId = elementId;
+			$container = $('#' + containerId);
+			draw();
+		},
+	};
 }());
 
 $(function(){	
 	$.getJSON("qif.json", function(data){
 		TRANSACTIONS.model.init();
 		TRANSACTIONS.model.setRecords(data.records);
-		TRANSACTIONS.ui.init($('#transactions'));
+		TRANSACTIONS.ui.init('transactions');
 	});
 });
